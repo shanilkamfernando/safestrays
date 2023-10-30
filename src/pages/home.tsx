afb7925b1a20559c902adff05d9fb52b5c9a8380
shanilka-fernando <shanilkaf.creativehub@gmail.com>
2023-10-30 23:17:07 +0530

    homepage

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,145 +1,195 @@
 import React from "react";
 import Image from "next/image";
-import  "./home.css"
+import "./home.css";
 
 function HomePage() {
   return (
-    <div className={'heroBanner'}>
-        shanilka
-      {/* <header className="absolute inset-x-0 top-0 z-50">
-        <nav
-          className="flex items-center justify-end p-6 lg:px-8 "
-          aria-label="Global"
-        >
-         
-          <div className="hidden lg:flex lg:gap-x-12 ">
-            <a
-              href="#"
-              className="text-sm font-semibold leading-6 text-gray-900"
-            >
-              HOME  
-            </a>
-            <a
-              href="#"
-              className="text-sm font-semibold leading-6 text-gray-900"
-            >
-              OUR STORY
-            </a>
-            <a
-              href="#"
-              className="text-sm font-semibold leading-6 text-gray-900"
-            >
-              SUPPORT
-            </a>
-            <a
-              href="#"
-              className="text-sm font-semibold leading-6 text-gray-900"
-            >
-              PROFILE
-            </a>
-          </div>
-        </nav>
-
-        <div className="lg:hidden" role="dialog" aria-modal="true">
-          <div className="fixed inset-0 z-50"></div>
-          <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-            <div className="flex items-center justify-between">
-              <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">Your Company</span>
+    <div>
+      <div className={"heroBanner"}>
+        <header className="absolute inset-x-0 top-0 z-50">
+          <nav
+            className="flex items-center justify-end p-6 lg:px-8 text-white"
+            aria-label="Global"
+          >
+            <div className="hidden lg:flex lg:gap-x-12 ">
+              <a
+                href="#"
+                className="text-sm font-semibold leading-6 text-white underline underline-offset-8"
+              >
+                HOME
               </a>
-             
+              <a
+                href="#"
+                className="text-sm font-semibold leading-6 text-white underline underline-offset-8"
+              >
+                OUR STORY
+              </a>
+              <a
+                href="#"
+                className="text-sm font-semibold leading-6 text-white underline underline-offset-8"
+              >
+                SUPPORT
+              </a>
+              <a
+                href="#"
+                className="text-sm font-semibold leading-6 text-white underline underline-offset-8"
+              >
+                PROFILE
+              </a>
             </div>
-            <div className="mt-6 flow-root ">
-              <div className="-my-6 divide-y divide-gray-500/10 ">
-                <div className="space-y-2 py-6">
-                  <a
-                    href="#"
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  >
-                    Product
-                  </a>
-                  <a
-                    href="#"
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  >
-                    Features
-                  </a>
-                  <a
-                    href="#"
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  >
-                    Marketplace
-                  </a>
-                  <a
-                    href="#"
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  >
-                    Company
-                  </a>
-                </div>
-                <div className="py-6">
-                  <a
-                    href="#"
-                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  >
-                    Log in
-                  </a>
+          </nav>
+
+          <div className="lg:hidden" role="dialog" aria-modal="true">
+            <div className="fixed inset-0 z-50"></div>
+            <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+              <div className="flex items-center justify-between">
+                <a href="#" className="-m-1.5 p-1.5">
+                  <span className="sr-only">Your Company</span>
+                </a>
+              </div>
+              <div className="mt-6 flow-root ">
+                <div className="-my-6 divide-y divide-gray-500/10 ">
+                  <div className="space-y-2 py-6">
+                    <a
+                      href="#"
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    >
+                      Product
+                    </a>
+                    <a
+                      href="#"
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    >
+                      Features
+                    </a>
+                    <a
+                      href="#"
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    >
+                      Marketplace
+                    </a>
+                    <a
+                      href="#"
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    >
+                      Company
+                    </a>
+                  </div>
+                  <div className="py-6">
+                    <a
+                      href="#"
+                      className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    >
+                      Log in
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </header>
+        </header>
 
-      <div className="relative isolate px-6 pt-14 lg:px-8">
-        <div
-          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-          aria-hidden="true"
-        >
-          <div className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"></div>
-        </div>
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Announcing our next round of funding.{" "}
-              <a href="#" className="font-semibold text-indigo-600">
-                <span className="absolute inset-0" aria-hidden="true"></span>
-                Read more <span aria-hidden="true">&rarr;</span>
-              </a>
-            </div>
+        <div className="h-[100vh] flex flex-col justify-center items-center">
+          <div className=" text-center">
+            <div className="text-[36px]">WELCOME TO</div>
+            <div className="text-[144px]">SafeStrays</div>
           </div>
-          <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Data to enrich your online business
-            </h1>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-              fugiat aliqua.
-            </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="#"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Get started
-              </a>
-              <a
-                href="#"
-                className="text-sm font-semibold leading-6 text-gray-900"
-              >
-                Learn more <span aria-hidden="true">→</span>
-              </a>
-            </div>
+          <div className=" rounded-xl border-[1px] border-white tracking-widest w-[20%] flex justify-center bg-[#C4C4C454] ">
+            Search
           </div>
         </div>
-        <div
-          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-          aria-hidden="true"
-        >
-          <div className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"></div>
+      </div>
+      <div className="bg-white text-black flex flex-col justify-center pb-10">
+        <div className="flex justify-center flex-col">
+          <div className="text-[30px] uppercase text-center tracking-[9px] border-b-2 border-black mx-[30rem]">
+            SafeStrays
+          </div>
+          {/* <div className="border-b-2 border-black pt-7 w-[400px] pb-10 "></div> */}
         </div>
-      </div> */}
+        <div className="text-black text-[16px]">
+          <div className="pt-[2rem] mx-32 text-center">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum
+          </div>
+          <div className="pt-[2rem] mx-32 text-center">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum
+          </div>
+        </div>
+      </div>
+      <div className="bg-white text-black flex flex-col justify-center pb-10">
+        <div className="flex justify-center flex-col pb-10">
+          <div className="text-[30px] uppercase text-center tracking-[9px] border-b-2 border-black mx-[20rem]">
+            Explore our services
+          </div>
+          {/* <div className="border-b-2 border-black pt-7 w-[400px] pb-10 "></div> */}
+        </div>
+        <div className=" grid grid-cols-4 gap-4 mx-[10rem] pt-10 ">
+          <div className={" firstCard"}>
+            <div className="flex flex-col h-[220px] justify-evenly items-center" >
+                <div className=" flex flex-col justify-center  p-6 text-center text-white uppercase font-bold  text-[14px]">
+                Stray dog sighting and health reporting
+                </div>
+              
+              <div className="bg-black text-white text-center rounded-lg uppercase w-[50%] text-[14px]">
+                view more
+              </div>
+            </div>
+          </div>
+          <div className={" secondCard"}>
+            <div className="flex flex-col h-[220px] justify-evenly items-center" >
+                <div className=" flex flex-col justify-center  p-6 text-center text-white uppercase font-bold  text-[14px]">
+                Stray dog sighting and health reporting
+                </div>
+              
+              <div className="bg-black text-white text-center rounded-lg uppercase w-[50%] text-[14px]">
+                view more
+              </div>
+            </div>
+          </div>
+          <div className={" thirdCard"}>
+            <div className="flex flex-col h-[220px] justify-evenly items-center" >
+                <div className=" flex flex-col justify-center  p-6 text-center text-white uppercase font-bold  text-[14px]">
+                Stray dog sighting and health reporting
+                </div>
+              
+              <div className="bg-black text-white text-center rounded-lg uppercase w-[50%] text-[14px] ">
+                view more
+              </div>
+            </div>
+          </div>
+          <div className={" fourthCard"}>
+            <div className="flex flex-col h-[220px] justify-evenly items-center" >
+                <div className=" flex flex-col justify-center  p-6 text-center text-white uppercase font-bold  text-[14px]">
+                Stray dog sighting and health reporting
+                </div>
+              
+              <div className="bg-black text-white text-center rounded-lg uppercase w-[50%] text-[14px] ">
+                view more
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-black p-5 flex justify-center items-center flex-col">
+        <div className="tracking-[6px] uppercase">SafeStrays</div>
+      <div className="leading-relaxed">copyright @ SafeStrays 2023</div>
+      </div>
     </div>
   );
 }
